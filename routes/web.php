f3c1fb36eb527272062ceef27a90c1ccf592e5c1
anthony canol <hay.an2ny@gmail.com>
2022-12-05 22:20:20 -0800

    layout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//default sample
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
